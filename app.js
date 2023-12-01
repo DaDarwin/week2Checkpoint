1e678651e88568233512a54b3d407ff063a5372c
Darien <darienjensen@outlook.com>
2023-12-01 14:35:20 -0700

    better addupgrade()

--- app.js
+++ app.js
@@ -7,26 +7,34 @@ let totalRocks = 0
 
 function collectRocks(){
     rocks += player.rocksOnClick
+    updateDom()
 }
 
-let upgrades = [
-    [
-        {name:'Pick',
-        qty:0,
-        amount:1,
-        price:5},
-        {name:'Drill',
-        qty:0,
-        amount:5,
-        price: 20},
-    ],
-   []
-]
+let upgrades = {
+        click:[{
+            name:'Pick',
+            kind:'click',
+            qty:0,
+            amount:1,
+            price:5},
+            {name:'Drill',
+            kind:'click',
+            qty:0,
+            amount:5,
+            price: 20},
+            ],
+        auto:[]
+}
 
-
-function addUpgrade(kind,upgradeName){
-    console.log(kind,upgradeName)
-    let addedUpgrade = upgrades[kind].find(upgrade => upgrade.name == upgradeName)
+function addUpgrade(kind, upgradeName){
+    console.log(kind, upgradeName) 
+    let type;
+    if(kind == 'click') type = upgrades.click
+    type.find(upgrade => upgrade.name == upgradeName)
+    console.log(type)
+    let addedUpgrade = type.find(upgrade => upgrade.name == upgradeName)
+    console.log(addedUpgrade)
+    find(upgrade => upgrade.name == upgradeName)
     console.log(addedUpgrade)
     if(rocks >= addedUpgrade.price){
         console.log(`Rocks: ${rocks} Upgrade: ${addedUpgrade}`)
@@ -40,7 +48,7 @@ function addUpgrade(kind,upgradeName){
 }
 
 function updateStats(kind, upgrade){
-    if(kind == 0){
+    if(kind == 'click'){
         player.rocksOnClick += upgrade.amount
     }
     else player.autoRocks += upgrade.amount
@@ -48,13 +56,33 @@ function updateStats(kind, upgrade){
 
 function updateDom(upgrade){
     document.getElementById('rockAmount').innerText = `Rocks: ${rocks}`
-    document.getElementById('clickRocks').innerText = `Rocks on Click${player.rocksOnClick}`
-    document.getElementById('autoRocks').innerText = `Passive Rocks: ${player.autoRocks}`
+    document.getElementById('clickRocks').innerText = `RPC: ${player.rocksOnClick}`
+    document.getElementById('autoRocks').innerText = `RPS: ${player.autoRocks}`
 
     if(upgrade){
     let upgradeElmChildren = document.getElementById(upgrade.name).children
-    upgradeElmChildren[0].innerText =`Name:${upgrade.name}`
-    upgradeElmChildren[1].innerText = `Qty: ${upgrade.qty}` 
-    upgradeElmChildren[2].innerText = `Click Power:${upgrade.qty * upgrade.amount}`
-    upgradeElmChildren[3].innerText = `Rock Cost:${upgrade.price} Rocks`
+    upgradeElmChildren[0].innerText =`${upgrade.name}:`
+    upgradeElmChildren[1].innerText = `Click Power:${upgrade.qty * upgrade.amount}`
+    upgradeElmChildren[2].innerText = `Qty: ${upgrade.qty}` 
+    upgradeElmChildren[3].innerText = `Rock Cost: ${upgrade.price} Rocks`
 }}
+
+function drawUpgrade(upgrade){
+    const upgradeELM = document.createElement('div')
+    upgradeELM.id = upgrade.name
+    upgradeELM.classList.add('row justify-content-center')
+
+    const nameELM = document.createElement('h4')
+    nameELM.classList.add('col-12')
+    const powerELM = document.createElement('h5')
+    powerELM.classList.add('col-6')
+    const qtyELM = document.createElement('h5')
+    upgradeELM.classList.add('col-6')
+    const priceELM = document.createElement('button')
+    priceELM.classList.add('col-6 btn btn-outline-info')
+    priceELM.onclick = addUpgrade(upgrade.name)
+
+
+}
+
+console.log(upgrades.find(type => type.upgrade == type.find(upgrade => upgrade.name == 'Pick')))
